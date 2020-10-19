c191658a8d2811fb3c0d6122546d2474ec65536b
Luiz Fernando Paulino Queiroz <luizfernando.lf355@gmail.com>
2020-10-19 01:34:55 -0300

    implementação do tableaux

--- src/semantics.kt
+++ src/semantics.kt
@@ -53,125 +53,74 @@ private fun satisfiability_check(formula: Formula, allAtoms: MutableSet<Formula>
     }
 }
 
-fun tableaux(formula: Formula): MutableMap<String, Boolean>? {
-    return when (formula){
-        // quando a fórmula é um átomo, ela já é satisfatível
-        is Atom -> mutableMapOf<String, Boolean>(formula.proposicao to true)
-        // transportando a fórmula para dentro de uma lista e chamando o solveTableaux
-        else -> {
-            val interpretation = solveTableaux(formula, null)
-            if (isNullInterpretation(interpretation))
-                null
-            else
-                complementaInterpretacao(formula, interpretation!!)
-        }
-    }
+fun tableaux(formula: Formula): MutableMap<String, Boolean>?{
+    val atoms = solveTableaux(formula, setOf<Formula>())
+    return complementaInterpretacao(atoms)
 }
 
-private fun solveTableaux(formula: Formula, interpretation: Map<String, Boolean>?): MutableMap<String, Boolean>?{
-    // criação de novos objetos(cópia) para evitar manipular os objetos que já estão na memória
-    val newInterpretation: MutableMap<String,Boolean> = mutableMapOf<String,Boolean>()
-    interpretation?.let { newInterpretation.putAll(it) }
-    var newformula = formula
-    println(newformula)
-    println(newInterpretation)
+private fun solveTableaux(formula: Formula, allAtoms: Set<Formula>?): MutableSet<Formula>{
+    val newAtoms: MutableSet<Formula> = mutableSetOf()
+    if (!allAtoms.isNullOrEmpty())
+        newAtoms.addAll(allAtoms as MutableSet<Formula>)
+//    println(formula)
+//    println(newAtoms)
 
-    when (newformula){
-        is Atom -> {
-            //caso já exista uma interpretação anterior onde este átomo é falso
-            return if (newInterpretation[newformula.proposicao] == false){
-                println("Solve Atom 1")
-                //o ramo é insatisfatível, logo esta interpretação é inválida
-                null
-            }else {
-                //se não, o átomo pode ser adicionado à interpretação como verdadeiro
-                newInterpretation[newformula.proposicao] = true
-                println("Solve Atom 2 $newInterpretation")
-                newInterpretation
+    return when(formula){
+        is Atom ->{
+            if (Not(formula) in newAtoms) {
+                mutableSetOf<Formula>()
+            } else {
+                newAtoms.add(formula)
+                newAtoms
             }
         }
         is Not -> {
-            return if (newformula.proposicao is Atom) {
-                // variável criada com a necessidade de um "cast"
-                val atom: Atom = newformula.proposicao as Atom
-                //caso já exista uma interpretação anterior onde este átomo é verdadeiro
-                if (newInterpretation[atom.proposicao] == true) {
-                    //o ramo é insatisfatível, logo esta interpretação é inválida
-                    println("Solve Not 1")
-                    println(newInterpretation)
-                    null
+            if (formula.proposicao is Atom){
+                if (formula.proposicao in newAtoms){
+                    mutableSetOf<Formula>()
                 } else {
-                    println("Solve Not 2")
-                    newInterpretation[atom.proposicao] = false
-                    newInterpretation
+                    newAtoms.add(formula)
+                    newAtoms
                 }
-            } else{
-                // caso o que esteja dentro da negação não seja átomo, deve-se aplicar equivalência
-                // de deMorgan
-                println("Solve Not 3")
-                newformula = deMorgan(newformula)
-                solveTableaux(newformula,newInterpretation)
+            } else {
+                val newformula = deMorgan(formula)
+                solveTableaux(newformula, newAtoms)
             }
         }
         is And -> {
-            var interpretationAux = solveTableaux(newformula.left, solveTableaux(newformula.right,newInterpretation))
-
-            return if (isNullInterpretation(interpretationAux)){
-                println("Solve And 1")
-                interpretationAux = solveTableaux(newformula.right, solveTableaux(newformula.left, newInterpretation))
-                if (isNullInterpretation(interpretationAux))
-                    null
-                else
-                    solveTableaux(newformula.left, interpretationAux)
-            } else {
-                println("Solve And 2")
-                interpretationAux = solveTableaux(newformula.right, interpretationAux)
-                interpretationAux
+            var atomsAux = solveTableaux(formula.left, newAtoms)
+            if (atomsAux.isNotEmpty()){
+                atomsAux = solveTableaux(formula.right, atomsAux) as MutableSet<Formula>
+                if (atomsAux.isEmpty()){
+                    atomsAux = solveTableaux(formula.right,newAtoms)
+                    if (atomsAux.isNotEmpty()) {
+                        atomsAux = solveTableaux(formula.left, atomsAux)
+                    }
+                }
             }
+            atomsAux
         }
         is Or -> {
-            val interpretationAux = solveTableaux(newformula.left, newInterpretation)
-
-            return if (isNullInterpretation(interpretationAux)) {
-                println("Solve Or 1")
-                solveTableaux(newformula.right, newInterpretation)
-            } else{
-                println("Solve Or 2")
-                interpretationAux
+            val atomsAux = solveTableaux(formula.left, newAtoms)
+            if (atomsAux.isEmpty()) {
+                solveTableaux(formula.right, newAtoms)
+            }
+            else {
+                atomsAux
             }
         }
-        is Implies -> {
-            val interpretationAux = solveTableaux(Not(newformula.left), newInterpretation)
-
-            return if (isNullInterpretation(interpretationAux)) {
-                println("Solve Implies 1")
-                solveTableaux(newformula.right, newInterpretation)
-            } else{
-                println("Solve Implies 2")
-                interpretationAux
-            }
-        }
-        is OnlyIf -> {
-            return solveTableaux(Or(And(newformula.left,newformula.right),
-                            And(Not(newformula.left),Not(newformula.right))),newInterpretation)
-        }
-        is Xor -> {
-            return solveTableaux(Or(And(Not(newformula.left),newformula.right),
-                    And(newformula.left,Not(newformula.right))),newInterpretation)
-        }
-        else -> {
-            println("erro1")
-            return null
-        }
+        else -> mutableSetOf<Formula>()
     }
+
 }
 
-fun complementaInterpretacao(formula: Formula, interpretation: Map<String, Boolean>): MutableMap<String, Boolean>?{
-    val atoms = atoms(formula)
-    val newInterpretation = interpretation.toMutableMap()
+fun complementaInterpretacao(atoms: Set<Formula>): MutableMap<String, Boolean>?{
+    val newInterpretation = mutableMapOf<String, Boolean>()
     for (i in atoms){
-        if (interpretation[i.toString()] != false){
+        if (i is Atom){
             newInterpretation[i.toString()] = true
+        } else if (i is Not){
+            newInterpretation[i.proposicao.toString()] = false
         }
     }
     return newInterpretation
--- src/test.kt
+++ src/test.kt
@@ -4,24 +4,36 @@ import torreGSM.iniciarTorreGSM
 * Siga as instruções do console para que o algoritmo retorne a resposta desejada
 * */
 fun main(){
-//    iniciarTorreGSM()
+    iniciarTorreGSM()
 //    val m = mutableMapOf<String, Boolean>("a" to false,
 //            "a" to true)
 //    println(m)
+//
+//    val p1 = Atom("p1")
+//    val q1 = Atom("q1")
+//    val r1 = Atom("r1")
+//    val p2 = Atom("p2")
+//    val q2 = Atom("q2")
+//    val r2 = Atom("r2")
+//    val p3 = Atom("p3")
+//    val q3 = Atom("q3")
+//    val r3 = Atom("r3")
+//
+//    val s1 = Atom("s")
+//    val t1 = Atom("t")
+//
+//    val k1 = And(And(Not(Or(p2,p3)),Not(p1)),Or(Or(p2,p3),p1))
+//    val k2 = And(And(Not(Or(p2,p3)),p1),Or(Or(p2,p3),p1))
+//    println(tableaux2(k1))
+//    println(tableaux2(k2))
+//
+//    val l1 = Or(Or(q1,q2),q3)
+//    val l2 = Not(Or(Or(q1,q2),q3))
+//
+//    println(tableaux2(l1))
+//    println(tableaux2(l2))
 
-    val p = Atom("p")
-    val q = Atom("q")
-    val r = Atom("r")
-    val s = Atom("s")
 
-    val k = Not(Or(p,q))
-    val l = Or(k,r)
-    val m = Or(p, Not(q))
-    val n = And(m,r)
-    val o = And(l,n)
-    val a = Or(o,s)
-
-    println(tableaux(o))
 
 }
 
